112493c8b9645c87d5632b8cfad002e421a244be
Paolo Chang <chanhwan.chang@gmail.com>
2021-06-26 23:27:37 -0400

    Add Blockchain module

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ Build a blockchain with a hash, mining, blocks and explorer, JSON API.
 
 - Validator in blockchain, miners, rewards, transaction fields
 - Communicate between different nodes in the decentralized peer to peer network
+
+### Tasks
+
+- [x] BLOCKCHAIN
+- [ ] EXPLORER
+- [ ] REST API
+- [ ] CLI
+- [ ] PERSISTENCE
+- [ ] MINING
+- [ ] TRANSACTIONS
+- [ ] WALLET
+- [ ] UNIT TESTING
+- [ ] P2P
+- [ ] SECURITY
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/paolochang/gocoin/blockchain"
+)
 
 func main(){
-	fmt.Println("Hello")
+	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second block")
+	chain.AddBlock("Third block")
+	chain.AddBlock("Forth block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("PrevHash: %s\n", block.PrevHash)
+	}
 }
