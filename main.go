0bfac6760ebab5673045176626a1c49ec63467b1
Paolo Chang <chanhwan.chang@gmail.com>
2021-06-28 23:08:11 -0400

    Add server-side rendering website

--- main.go
+++ main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/paolochang/gocoin/blockchain"
+	"log"
+	"net/http"
 )
 
+const port string = ":4000"
+
+func home (rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(rw, "Hello from home!")
+}
+
 func main(){
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second block")
-	chain.AddBlock("Third block")
-	chain.AddBlock("Forth block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("PrevHash: %s\n", block.PrevHash)
-	}
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on http://localhost%s\n",port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
