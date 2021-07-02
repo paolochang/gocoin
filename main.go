093aaf764b75d601d1c84bb537d4de766f06b610
Paolo Chang <chanhwan.chang@gmail.com>
2021-07-01 21:38:03 -0400

    Explorer: Refactoring

--- main.go
+++ main.go
@@ -1,47 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
+import "github.com/paolochang/gocoin/explorer"
 
-	"github.com/paolochang/gocoin/blockchain"
-)
-
-const (
-	port string = ":4000"
-	templateDir string = "templates/"
-)
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string
-	Blocks []*blockchain.Block
-}
-
-func home (rw http.ResponseWriter, r *http.Request) {
-	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	templates.ExecuteTemplate(rw, "home", data)
-}
-
-func add (rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		templates.ExecuteTemplate(rw, "add", nil)
-	case "POST":
-		r.ParseForm()
-		data := r.Form.Get("blockData")
-		blockchain.GetBlockchain().AddBlock(data)
-		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
-	}
-}
-
-func main(){
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-	fmt.Printf("Listening on http://localhost%s\n",port)
-	log.Fatal(http.ListenAndServe(port, nil))
+func main() {
+	explorer.Start()
 }
