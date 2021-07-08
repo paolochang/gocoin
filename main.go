a3a11d91b0353ddf9e2aaf7f3394143a05d20d25
Paolo Chang <chanhwan.chang@gmail.com>
2021-07-07 22:40:09 -0400

    REST API: Setup

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "github.com/paolochang/gocoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/paolochang/gocoin/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL string
+	Method string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription {
+		{
+			URL: "/",
+			Method: "GET",
+			Description: "See documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s", b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/",documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
+
