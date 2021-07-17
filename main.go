6c3042a1a5e5b6ada5657eb39312cad042d6e892
Paolo Chang <chanhwan.chang@gmail.com>
2021-07-16 23:14:01 -0400

    REST API: JSON Decode

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/paolochang/gocoin/blockchain"
+	"github.com/paolochang/gocoin/utils"
 )
 
 const port string = ":4000"
@@ -22,6 +25,10 @@ type URLDescription struct {
 	Method string `json:"method"`
 	Description string `json:"description"`
 	Payload string `json:"payload,omitempty"`
+}
+
+type AddBlockBody struct {
+	Message string
 }
 
 func documentation(rw http.ResponseWriter, r *http.Request) {
@@ -45,8 +52,23 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(rw).Encode(data) /* this line is equivalent to above three lines */
 }
 
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		// fmt.Println(addBlockBody)
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
 func main() {
 	http.HandleFunc("/",documentation)
+	http.HandleFunc("/blocks", blocks)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
