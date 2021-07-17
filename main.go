80fc51a228eba7c9bf56408217df183065bcf59b
Paolo Chang <chanhwan.chang@gmail.com>
2021-07-16 23:53:04 -0400

    REST API: NewServeMux

--- main.go
+++ main.go
@@ -1,75 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/paolochang/gocoin/blockchain"
-	"github.com/paolochang/gocoin/utils"
+	"github.com/paolochang/gocoin/explorer"
+	"github.com/paolochang/gocoin/rest"
 )
 
-const port string = ":4000"
-
-type URL string
-
-// by implementing method, type URL implement MarshalText interface 
-func (u URL) MarshalText() ([]byte, error) {
-	url := fmt.Sprintf("http://localhost%s%s", port, u)
-	return []byte(url), nil
-}
-
-type URLDescription struct {
-	URL URL `json:"url"`
-	Method string `json:"method"`
-	Description string `json:"description"`
-	Payload string `json:"payload,omitempty"`
-}
-
-type AddBlockBody struct {
-	Message string
-}
-
-func documentation(rw http.ResponseWriter, r *http.Request) {
-	data := []URLDescription {
-		{
-			URL: URL("/"),
-			Method: "GET",
-			Description: "See documentation",
-		},
-		{
-			URL: URL("/blocks"),
-			Method: "POST",
-			Description: "Add a block",
-			Payload: "data:string",
-		},
-	}
-	rw.Header().Add("Content-Type", "application/json")
-	// b, err := json.Marshal(data)
-	// utils.HandleErr(err)
-	// fmt.Fprintf(rw, "%s", b)
-	json.NewEncoder(rw).Encode(data) /* this line is equivalent to above three lines */
-}
-
-func blocks(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		rw.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
-	case "POST":
-		var addBlockBody AddBlockBody
-		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
-		// fmt.Println(addBlockBody)
-		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
-		rw.WriteHeader(http.StatusCreated)
-	}
-}
-
 func main() {
-	http.HandleFunc("/",documentation)
-	http.HandleFunc("/blocks", blocks)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	go explorer.Start(3000)
+	rest.Start(4000)
 }
 
---
+++ rest/rest.go
@@ -0,0 +1,85 @@
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/paolochang/gocoin/blockchain"
+	"github.com/paolochang/gocoin/utils"
+)
+
+var port string
+
+type url string
+
+// by implementing method, type URL implement MarshalText interface 
+func (u url) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
+type urlDescription struct {
+	URL url `json:"url"`
+	Method string `json:"method"`
+	Description string `json:"description"`
+	Payload string `json:"payload,omitempty"`
+}
+
+type addBlockBody struct {
+	Message string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []urlDescription {
+		{
+			URL: url("/"),
+			Method: "GET",
+			Description: "See documentation",
+		},
+		{
+			URL: url("/blocks"),
+			Method: "GET",
+			Description: "See all blocks",
+		},
+		{
+			URL: url("/blocks"),
+			Method: "POST",
+			Description: "Add a block",
+			Payload: "data:string",
+		},
+		{
+			URL: url("/blocks/{id}"),
+			Method: "GET",
+			Description: "See a block",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	// b, err := json.Marshal(data)
+	// utils.HandleErr(err)
+	// fmt.Fprintf(rw, "%s", b)
+	json.NewEncoder(rw).Encode(data) /* this line is equivalent to above three lines */
+}
+
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		var addBlockBody addBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
+func Start(aPort int) {
+	handler := http.NewServeMux()
+	port = fmt.Sprintf(":%d", aPort)
+	handler.HandleFunc("/",documentation)
+	handler.HandleFunc("/blocks", blocks)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, handler))
+}
