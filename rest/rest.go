bae4565f2024317617ead5177c3de9e7d7d472cc
Paolo Chang <chanhwan.chang@gmail.com>
2021-07-20 23:33:11 -0400

    REST API: Middlewares

--- rest/rest.go
+++ rest/rest.go
@@ -61,7 +61,6 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 			Description: "See a block",
 		},
 	}
-	rw.Header().Add("Content-Type", "application/json")
 	// b, err := json.Marshal(data)
 	// utils.HandleErr(err)
 	// fmt.Fprintf(rw, "%s", b)
@@ -71,7 +70,6 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 func blocks(rw http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		rw.Header().Add("Content-Type", "application/json")
 		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
 	case "POST":
 		var addBlockBody addBlockBody
@@ -94,9 +92,17 @@ func block(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(rw, r)
+	})
+}
+
 func Start(aPort int) {
 	router := mux.NewRouter()
 	port = fmt.Sprintf(":%d", aPort)
+	router.Use(jsonContentTypeMiddleware)
 	router.HandleFunc("/",documentation).Methods("GET")
 	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
 	router.HandleFunc("/blocks/{height:[0-9]+}", block).Methods("GET")
