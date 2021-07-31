549c6d613ccd8b991043f8ac76a1612b9ba2f0d5
Paolo Chang <chanhwan.chang@gmail.com>
2021-07-30 23:34:39 -0400

    CLI: Parsing Commands

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-	"github.com/paolochang/gocoin/rest"
+	"fmt"
+	"os"
 )
+
+func usage() {
+	fmt.Printf("Welcome to Go Coin\n\n")
+	fmt.Printf("Please us the following commands:\n\n")
+	fmt.Printf("explorer:		Start the HTML Explorer\n")
+	fmt.Printf("rest:				Start the REST API (recommended)\n\n")
+	os.Exit(0)
+}
 
 func main() {
 	// go explorer.Start(3000)
-	rest.Start(4000)
+	// rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
 }
 
